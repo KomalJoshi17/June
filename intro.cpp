83614474db8af9bab64096c5463d738fdc7fae09
Komal Joshi <moudgillkomal6@gmail.com>
2024-06-01 10:37:46 +0530

    first commit

---
+++ intro.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+    
+int sum(int a, int b){
+    int s=a+b;
+    return s;
+}
+int main(){
+    int a;
+    a=sum(10,15);
+    cout<<"Sum = "<<a;
+    return 0;
+}
+
