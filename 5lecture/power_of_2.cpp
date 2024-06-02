e1e3b7d445f5cbd46873ff99d1bafff82e1ff063
Komal Joshi <moudgillkomal6@gmail.com>
2024-06-02 15:40:01 +0530

    new file:   5lecture/power_of_2.cpp 	new file:   5lecture/power_of_2.exe 	new file:   5lecture/sum_is_k.cpp 	new file:   5lecture/sum_is_k.exe 	new file:   5lecture/tempCodeRunnerFile.cpp 	new file:   5lecture/while_loop.cpp 	new file:   5lecture/while_loop.exe

---
+++ 5lecture/power_of_2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+    
+int main(){
+    int n;
+    cin>>n;
+
+    while(n>1){
+        if(n%2!=0){
+            cout<<"Not Power"<<endl;
+            break;
+        }
+        n=n/2;
+    }
+    if(n==1){
+        cout<<"Power"<<endl;
+    }
+    return 0;
+}
---
+++ 5lecture/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+int i = 0;   
+    while(i <= n) {
+        cout<<i<<endl;
+        i++;
+    }
+    
