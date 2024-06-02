4e8d915cf5f5c2bb201b869e51508a1ed03f2f5d
Komal Joshi <moudgillkomal6@gmail.com>
2024-06-02 11:20:48 +0530

    new file:   4lecture/break.cpp 	new file:   4lecture/break.exe 	new file:   4lecture/continue.cpp 	new file:   4lecture/continue.exe 	new file:   4lecture/count_digits.cpp 	new file:   4lecture/count_digits.exe 	new file:   4lecture/divisible_by_7.cpp 	new file:   4lecture/divisible_by_7.exe 	new file:   4lecture/even_numbers.cpp 	new file:   4lecture/even_numbers.exe 	new file:   4lecture/fizzbuzz.cpp 	new file:   4lecture/fizzbuzz.exe 	new file:   4lecture/n_number.cpp 	new file:   4lecture/n_number.exe 	new file:   4lecture/play_with_loops.cpp 	new file:   4lecture/play_with_loops.exe 	new file:   4lecture/sum_of_digits.cpp 	new file:   4lecture/sum_of_digits.exe 	new file:   4lecture/table_of_4.cpp 	new file:   4lecture/table_of_4.exe 	new file:   4lecture/tempCodeRunnerFile.cpp 	new file:   4lecture/tempCodeRunnerFile.exe 	new file:   5lecture/sum_from_l_to_r.cpp 	new file:   5lecture/sum_from_l_to_r.exe 	new file:   5lecture/sum_of_digits.cpp 	new file:   5lecture/sum_of_digits.exe

---
+++ 4lecture/tempCodeRunnerFile.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+    
+int main(){
+    int number;
+    cin>>number;
+    int count=0;
+
+    for (int i=number;i>0;i/=10){
+        count++;
+
+    }
+    cout<<count;
+    return 0;
+}
