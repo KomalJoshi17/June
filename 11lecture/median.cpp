e044f65b8a9a463eaa34f554d779dd2bcc869edd
Komal Joshi <moudgillkomal6@gmail.com>
2024-06-04 18:33:22 +0530

    new file:   11lecture/actual_reverse.cpp 	new file:   11lecture/actual_reverse.exe 	modified:   11lecture/median.cpp 	modified:   11lecture/median.exe 	modified:   11lecture/odd_even_array.cpp 	modified:   11lecture/odd_even_array.exe 	modified:   11lecture/smallest_and_Second_Smallest.exe 	new file:   11lecture/subarray.cpp

--- 11lecture/median.cpp
+++ 11lecture/median.cpp
@@ -10,10 +10,11 @@ int main(){
         cin>>arr[i];
     }
 
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<' ';
+    if (n%2==1){
+        cout<<arr[n/2];
+    }else{
+        double ans = (arr[(n-1)/2]+arr[(n+1)/2])/2.0;
+        cout<<ans;
     }
-
-    
     return 0;
 }
--- 11lecture/odd_even_array.cpp
+++ 11lecture/odd_even_array.cpp
@@ -6,14 +6,28 @@ int main(){
     int n;
     cin>>n;
     int arr[n];
-    int even=arr[0];
-    int odd=arr[0];
+    int even[n];
+    int odd[n];
+    int j=0;
+    int k=0;
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-
     for(int i=0;i<n;i++){
-        cout<<arr[i]<<' ';
+        if(arr[i]%2==0){
+            even[j]=arr[i];
+            j++;
+        }else{
+            odd[k]=arr[i];
+            k++;
+        }
+    }
+    for (int i=0;i<j;i++){
+        cout<<even[i]<<" ";
+    }
+    cout<<endl;
+    for (int i=0;i<k;i++){
+        cout<<odd[i]<<" ";
     }
     return 0;
 }
