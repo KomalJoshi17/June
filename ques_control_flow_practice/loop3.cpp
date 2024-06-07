37b2937a448968ef858c5756ef758e670e3edb4d
Komal Joshi <moudgillkomal6@gmail.com>
2024-06-07 20:48:40 +0530

    new file:   ques_control_flow_practice/if_else.cpp 	new file:   ques_control_flow_practice/if_else.exe 	new file:   ques_control_flow_practice/loop1.cpp 	new file:   ques_control_flow_practice/loop1.exe 	new file:   ques_control_flow_practice/loop2.cpp 	new file:   ques_control_flow_practice/loop2.exe 	new file:   ques_control_flow_practice/loop3.cpp 	new file:   ques_control_flow_practice/square_star_rhombus_star.cpp 	new file:   ques_control_flow_practice/square_star_rhombus_star.exe 	new file:   ques_control_flow_practice/tempCodeRunnerFile.cpp

---
+++ ques_control_flow_practice/loop3.cpp
@@ -0,0 +1,1 @@
+// Write a C++ program to input two numbers from user and find LCM (Lowest Common Multiple).
---
+++ ques_control_flow_practice/square_star_rhombus_star.cpp
@@ -0,0 +1,35 @@
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+    
+// int main(){
+//     int n;
+//     cin>>n;
+//     for (int i=0;i<n;i++){
+//         for( int j=0; j<n;j++){
+//             cout<<"*"<<" ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+    
+int main(){
+    int n;
+    cin>>n;
+    for (int i=1;i<=n;i++){
+        for( int j=1; j<=n;j++){
+            if(i==1 || i==n || j==1 || j==n){
+                cout<<"*";
+            }else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}    
