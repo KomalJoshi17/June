8d0dc1f933993a4a3af3b2cb0583047d41bd7f92
Komal Joshi <moudgillkomal6@gmail.com>
2024-06-08 17:28:58 +0530

    modified:   ques_control_flow_practice/square_star_rhombus_star.cpp 	modified:   ques_control_flow_practice/square_star_rhombus_star.exe 	new file:   ques_control_flow_practice/traingle_star.cpp

--- ques_control_flow_practice/square_star_rhombus_star.cpp
+++ ques_control_flow_practice/square_star_rhombus_star.cpp
@@ -1,3 +1,4 @@
+// Write a C++ program to print a square star(*) pattern series of N rows.
 // #include<iostream>
 // #include<bits/stdc++.h>
 // using namespace std;
@@ -14,22 +15,94 @@
 //     return 0;
 // }
 
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
+
+
+// Write a C++ program to print a hollow square star(*) pattern series of N rows.
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
     
-int main(){
-    int n;
-    cin>>n;
-    for (int i=1;i<=n;i++){
-        for( int j=1; j<=n;j++){
-            if(i==1 || i==n || j==1 || j==n){
-                cout<<"*";
-            }else{
-                cout<<" ";
-            }
-        }
-        cout<<endl;
-    }
-    return 0;
-}    
+// int main(){
+//     int n;
+//     cin>>n;
+//     for (int i=1;i<=n;i++){
+//         for( int j=1; j<=n;j++){
+//             if(i==1 || i==n || j==1 || j==n){
+//                 cout<<"*";
+//             }else{
+//                 cout<<" ";
+//             }
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }   
+
+
+
+// Write a C++ program to print a hollow square star pattern with diagonals.
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+    
+// int main(){
+//     int n;
+//     cin>>n;
+//     for (int i=1;i<=n;i++){
+//         for( int j=1; j<=n;j++){
+//             if(i==1 || i==n || j==1 || j==n || i==j || i+j==n+1){
+//                 cout<<"*";
+//             }else{
+//                 cout<<" ";
+//             }
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+
+// Write a C++ program to print a rhombus star pattern of N rows.
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+    
+// int main(){
+//     int number;
+//     cin>>number;
+
+//     for(int i=1;i<=number;i++){
+//         for(int j=1;j<=number-i;j++){
+//             cout<<" ";
+//         }
+//         for(int j=1;j<=number;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+// Write a C++ program to print the inverted/mirrored rhombus star pattern of N rows.
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+    
+// int main(){
+//     int number;
+//     cin>>number;
+
+//     for(int i=1;i<=number;i++){
+//         for(int j=1;j<i;j++){
+//             cout<<" ";
+//         }
+//         for(int j=1;j<=number;j++){
+//             cout<<"*";
+//         }
+        
+//         cout<<endl;
+//     }
+//     return 0;
+// }
