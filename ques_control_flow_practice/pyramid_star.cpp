fb0a645f6c0e5964928793476373b3623b541326
Komal Joshi <moudgillkomal6@gmail.com>
2024-06-08 23:16:53 +0530

    modified:   ques_control_flow_practice/pyramid_star.cpp 	modified:   ques_control_flow_practice/pyramid_star.exe

--- ques_control_flow_practice/pyramid_star.cpp
+++ ques_control_flow_practice/pyramid_star.cpp
@@ -1,19 +1,67 @@
 // Write a C++ program to print Pyramid star( equilateral triangle ) pattern of N rows.
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
     
-int main(){
-    int n;
-    cin>>n;
-    for (int i=1;i<=n;i++){
-        for(int j=1;j<=n-i;j++){
-            cout<<" ";
-        }
-        for(int j=1;j<=2*i-1;j++){
-            cout<<"*";
-        }
-        cout<<endl;
-    }
-    return 0;
-}
+// int main(){
+//     int n;
+//     cin>>n;
+//     for (int i=1;i<=n;i++){
+//         for(int j=1;j<=n-i;j++){
+//             cout<<" ";
+//         }
+//         for(int j=1;j<=2*i-1;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+// Write a C++ program to print hollow pyramid star pattern (*) pattern of N rows.
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+    
+// int main(){
+//     int n;
+//     cin>>n;
+//     for (int i=1;i<=n;i++){
+//         for(int j=1;j<=n-i;j++){
+//             cout<<" ";
+//         }
+//         for(int j=1;j<=2*i-1;j++){
+//             if(j==1 || i==1 || j==2*i-1 || i==n){
+//                 cout<<"*";
+//             }
+//             else{
+//                 cout<<" ";
+//             }
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+
+// Write a C++ program to print inverted Pyramid star pattern for N rows.
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+    
+// int main(){
+//     int n;
+//     cin>>n;
+//     for (int i=1;i<=n;i++){
+//         for(int j=1;j<=i-1;j++){
+//             cout<<" ";
+//         }
+//         for(int j=1;j<=2*n-(2*i-1);j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
