46c5ea23f61db801d31f40a63fd3ef315e7b2863
Komal Joshi <moudgillkomal6@gmail.com>
2024-06-08 18:05:44 +0530

    modified:   ques_control_flow_practice/traingle_star.cpp 	new file:   ques_control_flow_practice/traingle_star.exe

---
+++ ques_control_flow_practice/traingle_star.cpp
@@ -0,0 +1,107 @@
+// Write a C++ program to print right triangle star pattern (*) pattern of N rows.
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+    
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+
+// Write a C++ program to print hollow right triangle star pattern (*) pattern of N rows.
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+    
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             if(i==1||j==i||j==1||i==n){
+//             cout<<"*";
+//         }
+//         else{
+//             cout<<" ";
+//         }
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+
+// Write a C++ program to print mirrored right triangle star pattern (*) pattern of N rows.
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+    
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n-i;j++){
+//             cout<<" ";
+//         }
+//         for(int j=1;j<=i;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+// Write a C++ program to print hollow mirrored right triangle star pattern (*) pattern of N rows.
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+    
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<n-i;j++){
+//             cout<<" ";
+//         }
+//         for(int j=1;j<=i;j++){
+//             if(i==1||j==i||j==1||i==n){
+//             cout<<"*";
+//         }
+//         else{
+//             cout<<" ";
+//         }
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+
+// Write a C++ program to print inverted right triangle star pattern (*) pattern of N rows.
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+    
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i+1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
