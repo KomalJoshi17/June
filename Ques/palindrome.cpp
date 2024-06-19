831df6d0e780cd0779700b92207a65ba326aea33
Komal Joshi <moudgillkomal6@gmail.com>
2024-06-19 15:50:11 +0530

    modified:   Ques/palindrome.cpp

--- Ques/palindrome.cpp
+++ Ques/palindrome.cpp
@@ -1,19 +1,19 @@
-// #include<iostream>
-// #include<bits/stdc++.h>
-// using namespace std;
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
     
-// int main(){
-//     int number1;
-//     cin>>number1;
-//     int original=number1;
-//     int reverse=0;
-//     for (int i=number1;i>0;i/=10){
-//         reverse=reverse*10+i%10;
-//     }
-//     if(original!=reverse){
-//         cout<<"Not Palindrome";
-//     }else{
-//         cout<<"Palindrome";
-//     }
-//     return 0;
-// }
+int main(){
+    int number1;
+    cin>>number1;
+    int original=number1;
+    int reverse=0;
+    for (int i=number1;i>0;i/=10){
+        reverse=reverse*10+i%10;
+    }
+    if(original!=reverse){
+        cout<<"Not Palindrome";
+    }else{
+        cout<<"Palindrome";
+    }
+    return 0;
+}
